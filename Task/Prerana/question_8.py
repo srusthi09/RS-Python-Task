0847f4db054d14a9b3ce1f884df3fe080cad7ddc
Preranadas2021 <preranadas2021@gmail.com>
2024-05-25 21:08:41 +0530

    question_8.py

--- Task/Prerana/question_8.py
+++ Task/Prerana/question_8.py
@@ -8,7 +8,6 @@ def count_frequency(arr):
             frequency_dict[element] = 1
     return frequency_dict
 
-# Example usage:
 arr = [1, 2, 3, 4, 2, 3, 1, 2, 4, 5, "robotics"]
 frequency = count_frequency(arr)
 print(frequency)
